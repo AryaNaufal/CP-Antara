de5b2d020456eaeb5563316873011e39e6bbc147
AryaNaufal <arya.naufal.andt@gmail.com>
2024-09-02 08:06:49 +0700

    fix: cuztomize slug name

--- #include/Components/PartnerSection.php
+++ #include/Components/PartnerSection.php
@@ -11,88 +11,141 @@
         <div class="col-lg-12 slider">
           <div class="owl-carousel owl-theme slide-track-reverse">
             <div class="slide">
-              <img src="assets/img/antara/mitra/Acnnewswire.png" alt="Partner">
+              <a href="https://www.acnnewswire.com/" target="_blank">
+                <img src="assets/img/antara/mitra/Acnnewswire.png" alt="Partner">
+              </a>
             </div>
             <div class="slide">
-              <img src="assets/img/antara/mitra/Afp.png" alt="Partner">
+              <a href="https://www.afp.com/" target="_blank">
+                <img src="assets/img/antara/mitra/Afp.png" alt="Partner">
+              </a>
             </div>
             <div class="slide">
-              <img src="assets/img/antara/mitra/Ap_News.png" alt="Partner">
+              <a href="https://www.ap.org/" target="_blank">
+                <img src="assets/img/antara/mitra/Ap_News.png" alt="Partner">
+              </a>
             </div>
             <div class="slide">
-              <img src="assets/img/antara/mitra/Asianet.png" alt="Partner">
+              <a href="https://www.nethighway.com/" target="_blank">
+                <img src="assets/img/antara/mitra/Asianet.png" alt="Partner">
+              </a>
             </div>
             <div class="slide">
-              <img src="assets/img/antara/mitra/Bernama.png" alt="Partner">
+              <a href="https://www.bernama.com/en/" target="_blank">
+                <img src="assets/img/antara/mitra/Bernama.png" alt="Partner">
+              </a>
             </div>
             <div class="slide">
-              <img src="assets/img/antara/mitra/Blommberg.png" alt="Partner">
+              <a href="https://www.bloomberg.com/asia" target="_blank">
+                <img src="assets/img/antara/mitra/Blommberg.png" alt="Partner">
+              </a>
             </div>
             <div class="slide">
-              <img src="assets/img/antara/mitra/Acnnewswire.png" alt="Partner">
+              <a href="https://www.acnnewswire.com/" target="_blank">
+                <img src="assets/img/antara/mitra/Acnnewswire.png" alt="Partner">
+              </a>
             </div>
             <div class="slide">
-              <img src="assets/img/antara/mitra/Afp.png" alt="Partner">
+              <a href="https://www.afp.com/" target="_blank">
+                <img src="assets/img/antara/mitra/Afp.png" alt="Partner">
+              </a>
             </div>
             <div class="slide">
-              <img src="assets/img/antara/mitra/Ap_News.png" alt="Partner">
+              <a href="https://www.ap.org/" target="_blank">
+                <img src="assets/img/antara/mitra/Ap_News.png" alt="Partner">
+              </a>
             </div>
             <div class="slide">
-              <img src="assets/img/antara/mitra/Asianet.png" alt="Partner">
+              <a href="https://www.nethighway.com/" target="_blank">
+                <img src="assets/img/antara/mitra/Asianet.png" alt="Partner">
+              </a>
             </div>
             <div class="slide">
-              <img src="assets/img/antara/mitra/Bernama.png" alt="Partner">
+              <a href="https://www.bernama.com/en/" target="_blank">
+                <img src="assets/img/antara/mitra/Bernama.png" alt="Partner">
+              </a>
             </div>
             <div class="slide">
-              <img src="assets/img/antara/mitra/Blommberg.png" alt="Partner">
+              <a href="https://www.bloomberg.com/asia" target="_blank">
+                <img src="assets/img/antara/mitra/Blommberg.png" alt="Partner">
+              </a>
             </div>
           </div>
         </div>
       </div>
+      
       <div class="row">
         <div class="col-lg-12 slider">
           <div class="owl-carousel owl-theme slide-track">
             <div class="slide">
-              <img src="assets/img/antara/mitra/Efe.png" alt="Partner">
+              <a href="https://efe.com/" target="_blank">
+                <img src="assets/img/antara/mitra/Efe.png" alt="Partner">
+              </a>
             </div>
             <div class="slide">
-              <img src="assets/img/antara/mitra/Kyodonews.png" alt="Partner">
+              <a href="https://www.kyodonews.jp/" target="_blank">
+                <img src="assets/img/antara/mitra/Kyodonews.png" alt="Partner">
+              </a>
             </div>
             <div class="slide">
-              <img src="assets/img/antara/mitra/Oana.png" alt="Partner">
+              <a href="https://www.oananews.org/" target="_blank">
+                <img src="assets/img/antara/mitra/Oana.png" alt="Partner">
+              </a>
             </div>
             <div class="slide">
-              <img src="assets/img/antara/mitra/Sputnik.png" alt="Partner">
+              <a href="https://sputnikglobe.com/" target="_blank">
+                <img src="assets/img/antara/mitra/Sputnik.png" alt="Partner">
+              </a>
             </div>
             <div class="slide">
-              <img src="assets/img/antara/mitra/Thomson_Reuters.png" alt="Partner">
+              <a href="https://www.thomsonreuters.com/en.html" target="_blank">
+                <img src="assets/img/antara/mitra/Thomson_Reuters.png" alt="Partner">
+              </a>
             </div>
             <div class="slide">
-              <img src="assets/img/antara/mitra/Ttxvn.png" alt="Partner">
+              <a href="https://vnanet.vn/" target="_blank">
+                <img src="assets/img/antara/mitra/Ttxvn.png" alt="Partner">
+              </a>
             </div>
             <div class="slide">
-              <img src="assets/img/antara/mitra/Xinhua_News_Agency.png" alt="Partner">
+              <a href="http://www.news.cn/" target="_blank">
+                <img src="assets/img/antara/mitra/Xinhua_News_Agency.png" alt="Partner">
+              </a>
             </div>
             <div class="slide">
-              <img src="assets/img/antara/mitra/Efe.png" alt="Partner">
+              <a href="https://efe.com/" target="_blank">
+                <img src="assets/img/antara/mitra/Efe.png" alt="Partner">
+              </a>
             </div>
             <div class="slide">
-              <img src="assets/img/antara/mitra/Kyodonews.png" alt="Partner">
+              <a href="https://www.kyodonews.jp/" target="_blank">
+                <img src="assets/img/antara/mitra/Kyodonews.png" alt="Partner">
+              </a>
             </div>
             <div class="slide">
-              <img src="assets/img/antara/mitra/Oana.png" alt="Partner">
+              <a href="https://www.oananews.org/" target="_blank">
+                <img src="assets/img/antara/mitra/Oana.png" alt="Partner">
+              </a>
             </div>
             <div class="slide">
-              <img src="assets/img/antara/mitra/Sputnik.png" alt="Partner">
+              <a href="https://sputnikglobe.com/" target="_blank">
+                <img src="assets/img/antara/mitra/Sputnik.png" alt="Partner">
+              </a>
             </div>
             <div class="slide">
-              <img src="assets/img/antara/mitra/Thomson_Reuters.png" alt="Partner">
+              <a href="https://www.thomsonreuters.com/en.html" target="_blank">
+                <img src="assets/img/antara/mitra/Thomson_Reuters.png" alt="Partner">
+              </a>
             </div>
             <div class="slide">
-              <img src="assets/img/antara/mitra/Ttxvn.png" alt="Partner">
+              <a href="https://vnanet.vn/" target="_blank">
+                <img src="assets/img/antara/mitra/Ttxvn.png" alt="Partner">
+              </a>
             </div>
             <div class="slide">
-              <img src="assets/img/antara/mitra/Xinhua_News_Agency.png" alt="Partner">
+              <a href="http://www.news.cn/" target="_blank">
+                <img src="assets/img/antara/mitra/Xinhua_News_Agency.png" alt="Partner">
+              </a>
             </div>
           </div>
         </div>
