e9d6184efa86bf100172d98bc0eca39eac0182f2
AryaNaufal <arya.naufal.andt@gmail.com>
2024-09-05 17:28:50 +0700

    🔧fix: adjust SERVER_NAME host and autoload base directory

--- #include/#class/Autoload.php
+++ #include/#class/Autoload.php
@@ -1,7 +1,7 @@
 <?php
 spl_autoload_register(function ($class) {
     $prefix = 'App\\';
-    $base_dir = __DIR__ . '\\';
+    $base_dir = __DIR__ . '/';
 
     $len = strlen($prefix);
     if (strncmp($prefix, $class, $len) !== 0) {
@@ -10,7 +10,10 @@ spl_autoload_register(function ($class) {
 
     $relative_class = substr($class, $len);
     $file = $base_dir . str_replace('\\', DIRECTORY_SEPARATOR, $relative_class) . '.php';
-    //echo $file;
+
+    // Debugging: Tampilkan file yang sedang dicari
+    // echo "Looking for file: $file\n";
+
     if (file_exists($file)) {
         require $file;
     }
--- #include/config.php
+++ #include/config.php
@@ -4,8 +4,8 @@ define("TEMPLATE_PARSIAL", 1);
 define("DB_SERVER", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");
-define("DB_NAME", "cp_antara");
+define("DB_NAME", "");
 define("DB_PORT", "3306");
 
-define("SERVER_NAME", "http://localhost/CP-Antara/");
+define("SERVER_NAME", "http://" . $_SERVER['HTTP_HOST'] . "/CP-Antara/");
 define("ROOT_PATH", $_SERVER['DOCUMENT_ROOT'] . "/CP-Antara");
